57d53eac1d59b3cb4732c53429321f92b8d5cfb9
raccoonmin123 <ckmckmckm243@gmail.com>
2025-06-23 22:42:42 +0900

    Revert "로그인 , 지도"

--- starView/src/main/java/com/example/demo/mapper/MemberDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mapper;
-
-import com.example.demo.DTO.MemberDTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MemberDAO {
-    String loginCheck(MemberDTO dto);
-}
